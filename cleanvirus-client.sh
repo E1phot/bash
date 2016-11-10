c48f0ae365c0fd9e2369868e7d5cb12c3594cd43
E1phot <iiogorodov@gmail.com>
2016-11-10 15:41:26 +0400

    Update cleanvirus-client.sh

--- cleanvirus-client.sh
+++ cleanvirus-client.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 wineserver stop
 kill -9 `ps aux | grep "\.exe" | awk '{print $2}'`
-filesize=`find /shared-from-server/Documents/public/ -type f -name "*.exe" -exec ls -la {} \; | cut -d" " -f5 | sort | uniq -c | cut -d" " -f6`
-find /shared-from-server/Documents/ -type f -size ${filesize}c -a \( -name "*.exe" -o -name "*.pif" -o -name "*.scr" -o -name "*.bat" \) -exec rm -f {} \;
+filesize=`find /shared/Documents/public/ -type f -name "*.exe" -exec ls -la {} \; | cut -d" " -f5 | sort | uniq -c | cut -d" " -f6`
+find /shared/Documents/ -type f -size ${filesize}c -a \( -name "*.exe" -o -name "*.pif" -o -name "*.scr" -o -name "*.bat" \) -exec rm -f {} \;
 find /home/ -type f -size ${filesize}c -a \( -name "*.exe" -o -name "*.pif" -o -name "*.scr" -o -name "*.bat" \) -exec rm -rf {} \;
-find /shared-from-server/Documents/ /home/ -type f -size 520192c -a \( -name "*.exe" -o -name "*.pif" -o -name "*.scr" -o -name "*.bat" \) -exec rm -f {} \;
+find /shared/Documents/ /home/ -type f -size 520192c -a \( -name "*.exe" -o -name "*.pif" -o -name "*.scr" -o -name "*.bat" \) -exec rm -f {} \;
 for i in *.vuz *.zhp *.scr *.igt *.hiz *.xyq *.qcp *.twp *.cfh *.phy *.uwx *.rje *.eaw *.eif *.twp;
 do
 find /home/ -type f -name ${i} -exec rm -f {} \;
